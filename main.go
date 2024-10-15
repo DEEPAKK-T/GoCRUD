54ff805aa0aa1060eeab6c08f549063e3267c519
DeepakKt <deepakkt2032@gmail.com>
2024-10-15 19:59:17 +0530

    Experimentation with CURD enhancements and optimizations - Initial commit

--- main.go
+++ main.go
@@ -13,56 +13,67 @@ import (
 var todos []int
 
 func main() {
-	fmt.Println("Backend API to manage Todo items")
+
+	fmt.Println("Starting main function")
+
 	router := mux.NewRouter()
 
 	todos = append(todos, 1)
 	todos = append(todos, 2)
+	todos = append(todos, 3)
 
-	fmt.Printf("\n todos %v", todos)
-
-	//CRUD
 	router.HandleFunc("/todos", getTodos).Methods("GET")
-	router.HandleFunc("/todos", createTodo).Methods("POST")
-	router.HandleFunc("/todo/{id}", getTodo).Methods("GET")
-	router.HandleFunc("/todo/{id}", updateTodo).Methods("UPDATE")
-	router.HandleFunc("/todo/{id}", deleteTodo).Methods("DELETE")
+	router.HandleFunc("/todos", addTodos).Methods("POST")
+	router.HandleFunc("/todos/{id}", deleteTodo).Methods("DELETE")
+	router.HandleFunc("/todos/{id}", getTodoById).Methods("GET")
 
-	fmt.Println("Server listening on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	fmt.Println("Server listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("implement")
+	fmt.Println("Implementing getTodos")
+	//NewEncoder(w) , which will write the JSON output to the response writer w .
+	//The person struct was encoded to JSON and written to the response using encoder
 	json.NewEncoder(w).Encode(todos)
-	w.WriteHeader(200)
+	// w.WriteHeader(200)
 }
 
-func createTodo(w http.ResponseWriter, r *http.Request) {
+func addTodos(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Implementing create todo")
+
 	todos = append(todos, len(todos)+1)
-	fmt.Printf("\nTODO list %v", todos)
-	w.WriteHeader(200)
-}
-
-func getTodo(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id := params["id"]
-	fmt.Printf("\n Id is %v", id)
-	w.Write([]byte(id))
-}
-
-func updateTodo(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 
 func deleteTodo(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	if id > len(todos)-1 {
-		w.Write([]byte("Invalid ID"))
+
+	incomingId := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(incomingId)
+	fmt.Printf("Incoming Id to delete %v", id)
+
+	if id >= len(todos) {
+		w.Write([]byte("Invalid Id"))
 		return
 	}
+
 	todos = append(todos[:id], todos[id+1:]...)
-	fmt.Printf("\nTODO list %v", todos)
+	fmt.Printf("Updated todos %v", todos)
 	w.WriteHeader(200)
+}
+
+func getTodoById(w http.ResponseWriter, r *http.Request) {
+
+	incomingId := mux.Vars(r)["id"]
+	id, _ := strconv.Atoi(incomingId)
+	fmt.Printf("Incoming Id to delete %v", id)
+
+	if id < 1 || id >= len(todos)+1 {
+		w.Write([]byte("Invalid Id"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(todos[id-1])
+	// w.WriteHeader(200)
 }
